ff3fa69347edcd5c7d1f766fd67916ad09a9528c
marialuizacosta <88732902+marialuizacosta@users.noreply.github.com>
2021-10-14 14:28:13 -0300

    Rename Questao5.java.txt to Questao5.java

--- Questao5.java
+++ Questao5.java
@@ -1,7 +1,7 @@
 public class Questao5 {
   public static void main(String [] args)throws Exception {
 
- Aluno aluno = new Aluno("Maria Luíza Costa Andrade", "69235784606");
+ Aluno aluno = new Aluno("Maria LuÃ­za Costa Andrade", "69235784606");
     
 System.out.println("\nNome: " +aluno.nome);
 System.out.println("CPF: " +aluno.cpf);
@@ -12,14 +12,14 @@ aluno.nota3 = 9.9;
 
 aluno.calcularMedia();
 double media = aluno.calcularMedia();
-System.out.println("\nMédia: "+media);
+System.out.println("\nMÃ©dia: "+media);
 
 aluno.nota1 = 8.1;
 aluno.nota2 = 6.8;
 aluno.nota3 = 2.5;
 
 aluno.calcularMedia();
-System.out.println("Média: "+aluno.calcularMedia());
+System.out.println("MÃ©dia: "+aluno.calcularMedia());
 
 
 
